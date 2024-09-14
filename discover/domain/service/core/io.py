60b371911d2ddc6b1fd6f73dc22c4fe85530f3be
John James <john@variancexplained.com>
2024-09-14 14:28:26 -0400

    recovered WriteTask

--- discover/domain/service/core/io.py
+++ discover/domain/service/core/io.py
@@ -11,7 +11,7 @@
 # URL        : https://github.com/variancexplained/appvocai-discover                               #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Friday September 13th 2024 07:13:32 pm                                              #
-# Modified   : Saturday September 14th 2024 05:25:00 am                                            #
+# Modified   : Saturday September 14th 2024 02:28:14 pm                                            #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2024 John James                                                                 #
@@ -22,6 +22,7 @@ from discover.domain.base.task import Task
 from discover.domain.value_objects.config import DataConfig
 from discover.domain.value_objects.context import Context
 from discover.domain.value_objects.lifecycle import Stage
+from discover.infra.monitor.profiler import profiler
 
 
 # ------------------------------------------------------------------------------------------------ #
@@ -77,6 +78,7 @@ class ReadTask(Task):
         """
         return self._stage
 
+    @profiler
     def run(self, *args, **kwargs) -> Any:
         """
         Executes the task by retrieving data from the repository based on the stage and name
@@ -98,3 +100,53 @@ class ReadTask(Task):
         return self._data_config.repo.get(
             stage=self._data_config.stage, name=self._data_config.name
         )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class WriteTask(Task):
+    """
+    A task class responsible for writing data to a repository. This class extends the base `Task` class
+    and is used in a data pipeline to handle writing operations to a repository.
+
+    Attributes:
+        data_config (DataConfig): Configuration object containing details about the data repository,
+                                  stage, and other necessary configuration parameters.
+        context (Context): An object providing the context in which this task is running, including
+                           information such as the current stage of the pipeline.
+        kwargs (dict): Additional keyword arguments passed during task initialization, allowing flexibility
+                       for extra parameters needed during the write operation.
+
+    Methods:
+        stage() -> Stage:
+            Returns the current pipeline stage.
+
+        run(data: Any) -> Any:
+            Executes the task to write data to the configured repository. The `data` argument represents the
+            data to be written. The method updates the repository with the provided data and any extra
+            configuration provided through the `kwargs`.
+
+            Args:
+                data (Any): The data to be written to the repository.
+
+            Returns:
+                Any: The same data passed as input, allowing for chained operations or further processing.
+    """
+
+    def __init__(self, data_config: DataConfig, context: Context, **kwargs) -> None:
+        self._data_config = data_config
+        self._context = context
+        self._stage = context.stage
+        self._kwargs = kwargs
+
+    def stage(self) -> Stage:
+        return self._stage
+
+    def run(self, data: Any) -> Any:
+        super().run(data=data)
+        self._data_config.repo.add(
+            stage=self._data_config.stage,
+            name=self._data_config.name,
+            data=data,
+            **self._kwargs,
+        )
+        return data
