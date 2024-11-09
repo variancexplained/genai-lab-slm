4f4c6b8b795909d7fe53765f9233a78e5e603abc
John James <john@variancexplained.com>
2024-11-09 06:49:24 -0500

    eda overview

--- discover/app/eda.py
+++ discover/app/eda.py
@@ -4,14 +4,14 @@
 # Project    : AppVoCAI-Discover                                                                   #
 # Version    : 0.1.0                                                                               #
 # Python     : 3.10.14                                                                             #
-# Filename   : /discover/app/analyzer.py                                                           #
+# Filename   : /discover/app/eda.py                                                                #
 # ------------------------------------------------------------------------------------------------ #
 # Author     : John James                                                                          #
 # Email      : john@variancexplained.com                                                           #
 # URL        : https://github.com/variancexplained/appvocai-discover                               #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Friday October 18th 2024 11:07:32 am                                                #
-# Modified   : Tuesday October 29th 2024 07:59:01 pm                                               #
+# Modified   : Saturday November 9th 2024 05:36:33 am                                              #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2024 John James                                                                 #
@@ -37,12 +37,54 @@ from discover.infra.utils.visual.print import Printer
 # ------------------------------------------------------------------------------------------------ #
 printer = Printer()
 
+columns = [
+    "id",
+    "app_id",
+    "app_name",
+    "category",
+    "author",
+    "rating",
+    "content",
+    "vote_count",
+    "vote_sum",
+    "date",
+    "enrichment_meta_review_age",
+    "enrichment_meta_review_length",
+    "enrichment_meta_review_month",
+    "enrichment_meta_review_day_of_week",
+    "enrichment_meta_review_hour",
+    "enrichment_sentiment",
+    "enrichment_sentiment_classification",
+    "enrichment_tqa_score_final",
+    "enrichment_pct_deviation_rating",
+    "enrichment_pct_deviation_review_length",
+    "enrichment_pct_deviation_sentiment",
+    "enrichment_pct_deviation_tqa_score_final",
+    "pos_n_nouns",
+    "pos_n_verbs",
+    "pos_n_adjectives",
+    "pos_n_adverbs",
+    "pos_p_nouns",
+    "pos_p_verbs",
+    "pos_p_adjectives",
+    "pos_p_adverbs",
+    "stats_char_count",
+    "stats_unique_word_count",
+    "stats_unique_word_proportion",
+    "tqm_pos_count_score",
+    "tqm_pos_diversity_score",
+    "tqm_structural_complexity_score",
+    "tqm_pos_intensity_score",
+]
+
 
 # ------------------------------------------------------------------------------------------------ #
-class Analyzer:
+class EDA:
     """"""
 
-    def __init__(self) -> None:
+    def __init__(self, df: pd.DataFrame) -> None:
+        self._df = df[columns]
+        self._df = self._df.convert_dtypes(convert_string=True)
         self._distribution = Distribution()
         self._describe = Describe()
         self._freqdist = FreqDist()
@@ -54,24 +96,26 @@ class Analyzer:
         self._multicount = MultivariateCount()
         self._multimean = MultivariateStatisticsMean()
 
-    def info(self, df: pd.DataFrame) -> None:
-        self._overview = Overview(data=df)
+    def info(self) -> None:
+        self._overview = Overview(data=self._df)
         return self._overview.info()
 
-    def overview(self, df: pd.DataFrame) -> None:
-        n = df.shape[0]
-        p = df.shape[1]
-        n_auth = df["author"].nunique()
-        n_auth_inf = df.loc[df["vote_count"] > 0].nunique()
-        n_apps = df["app_id"].nunique()
-        n_categories = df["category"].nunique()
-        mem = df.memory_usage(deep=True)
-        dt_first = df["date"].min()
-        dt_last = df["date"].max()
+    def overview(self) -> None:
+        n = self._df.shape[0]
+        p = self._df.shape[1]
+        n_auth = self._df["author"].nunique()
+        n_auth_inf = self._df.loc[self._df["vote_count"] > 0]["author"].nunique()
+        n_repeat_auth = (self._df["author"].value_counts() > 1).sum()
+        n_apps = self._df["app_id"].nunique()
+        n_categories = self._df["category"].nunique()
+        mem = self._df.memory_usage(deep=True).sum().sum()
+        dt_first = self._df["date"].min()
+        dt_last = self._df["date"].max()
         d = {
             "Number of Reviews": n,
-            "Number of Authors": n_auth,
-            "Number of Authors with Influence": n_auth_inf,
+            "Number of Reviewrs": n_auth,
+            "Number of Repeat Reviewers": n_repeat_auth,
+            "Number of Influential Reviewers": n_auth_inf,
             "Number of Apps": n_apps,
             "Number of Categories": n_categories,
             "Features": p,
@@ -88,7 +132,7 @@ class Analyzer:
 
     @property
     def describe(self) -> Describe:
-        return self._discribe
+        return self._describe
 
     @property
     def freqdist(self) -> FreqDist:
--- discover/flow/enrich/deviation/task.py
+++ discover/flow/enrich/deviation/task.py
@@ -11,7 +11,7 @@
 # URL        : https://github.com/variancexplained/appvocai-discover                               #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Thursday November 7th 2024 11:25:40 pm                                              #
-# Modified   : Thursday November 7th 2024 11:31:05 pm                                              #
+# Modified   : Saturday November 9th 2024 02:06:01 am                                              #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2024 John James                                                                 #
@@ -28,30 +28,25 @@ from discover.infra.service.logging.task import task_logger
 class ComputePercentDeviationTask(Task):
     """
     A task to compute the percent deviation of a specified column from the average
-    and store the result in a new column.
+    grouped by a given level and store the result in a new column.
 
     Attributes:
         column (str): The name of the column for which to compute percent deviation.
         new_column (str): The name of the new column to store the percent deviation values.
+        by (str): The level by which the average is computed for the deviation calculation.
     """
 
-    def __init__(self, column: str, new_column: str) -> None:
-        """
-        Initializes the ComputePercentDeviationTask with the specified column and new column name.
-
-        Args:
-            column (str): The column to analyze for percent deviation.
-            new_column (str): The column to contain the calculated percent deviation.
-        """
+    def __init__(self, column: str, new_column: str, by: str = "category") -> None:
         super().__init__()
         self._column = column
         self._new_column = new_column
+        self._by = by
 
     @task_logger
     def run(self, data: DataFrame) -> DataFrame:
         """
         Computes the percent deviation of the specified column from the average
-        and adds it as a new column to the DataFrame.
+        grouped by the 'by' variable and adds it as a new column to the DataFrame.
 
         Args:
             data (DataFrame): The input PySpark DataFrame containing the data.
@@ -59,12 +54,19 @@ class ComputePercentDeviationTask(Task):
         Returns:
             DataFrame: The PySpark DataFrame with the new column containing percent deviation.
         """
-        # Calculate the average of the specified column
-        avg_value = data.select(F.avg(self._column)).first()[0]
+        # Calculate the average of the specified column, grouped by the 'by' variable
+        avg_values = data.groupBy(self._by).agg(F.avg(self._column).alias("avg_value"))
+
+        # Join the average values back to the original DataFrame
+        data = data.join(avg_values, on=self._by, how="left")
 
         # Compute the percent deviation and add it as a new column
         data = data.withColumn(
-            self._new_column, ((F.col(self._column) - avg_value) / avg_value) * 100
+            self._new_column,
+            ((F.col(self._column) - F.col("avg_value")) / F.col("avg_value")) * 100,
         )
+
+        # Drop the temporary 'avg_value' column
+        data = data.drop("avg_value")
 
         return data
